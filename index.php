d931ae2a324d18ca68b25806e3f1c3dc216f29fc
antoineegret <a.egret@laposte.net>
2019-12-10 12:05:24 +0100

    exo4 partie4 php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$a = '36';
+$b = '36';
+if ($a > $b) {
+  echo "Le premier nombre est plus grand";
+}
+if ($a < $b) {
+  echo "Le premier nombre est plus petit";
+}
+if ($a == $b) {
+  echo "les deux nombre sont identique";
+}
+ ?>
